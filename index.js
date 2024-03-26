241a9a5ca639ec2c4f9d4a1dabe94aac6c3c1204
Efosa <okoobohefosa@gmail.com>
2024-03-26 03:56:44 +0100

    fix port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8482452;
 
 app.get('/', (request, response) => {
     console.log("Running...")
